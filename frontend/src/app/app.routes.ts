f790f5211f2b6ffbbb8f79a1cad9700d5b04bc83
geth2a93 <alecbennardonew@gmail.com>
2026-04-25 21:51:29 -0700

    Fixed deletion merge issue

--- frontend/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { BlogComponent } from './blog.component';
-
-export const routes: Routes = [
-  { path: 'blog', component: BlogComponent },
-  { path: '', redirectTo: 'blog', pathMatch: 'full' }
-];
