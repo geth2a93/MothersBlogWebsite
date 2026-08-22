6d67411fc2e22fdff1101cbb15130b8f61d64e54
geth2a93 <alecbennardonew@gmail.com>
2026-08-21 17:06:24 -0700

    Update AdminEditBooks.jsx

--- frontend/src/admin/AdminEditBooks.jsx
+++ frontend/src/admin/AdminEditBooks.jsx
@@ -10,7 +10,7 @@ export default function AdminEditBooks() {
   const fetchBooks = async () => {
     try {
       const response = await fetch(
-        "${import.meta.env.VITE_API_URL}/admin/displayallbooks",
+        "/admin/displayallbooks",
         {
           credentials: "include",
         }
