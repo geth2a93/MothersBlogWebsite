ca5d8d5b029018d65f53c2b79b948fcdb1c13e27
geth2a93 <alecbennardonew@gmail.com>
2026-09-03 23:05:03 -0700

    missed import refs

--- frontend/src/dashboard/email/DisplayEmail.jsx
+++ frontend/src/dashboard/email/DisplayEmail.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../display.css";
+import "../css/display.css";
 
 function DisplayEmails() {
   const [emails, setEmails] = useState([]);
