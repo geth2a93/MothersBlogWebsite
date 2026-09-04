8aeccd60686646aa3885c2d0e33daca27356f2c0
geth2a93 <alecbennardonew@gmail.com>
2026-09-04 08:05:26 -0700

    dot

--- frontend/src/dashboard/blogs/EditBlog.jsx
+++ frontend/src/dashboard/blogs/EditBlog.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import "./css/editor.css";
+import "../css/editor.css";
 import RichTextEditor from "./RichTextEditor";
 
 import {
