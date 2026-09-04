268dfa101b042ff7f5b529897c5bf05a4c2e5d86
geth2a93 <alecbennardonew@gmail.com>
2026-09-04 13:05:57 -0700

    test

--- backend/src/website/functions.py
+++ backend/src/website/functions.py
@@ -74,7 +74,7 @@ def get_all_books(page, per_page=5):
                 "date_displayed": b.publish_date_displayed
             })
 
-    return {"books": data,
+    return {"data": data,
         "has_next": pagination.has_next,
         "page": page}
 
@@ -96,7 +96,7 @@ def get_books_by_genre(genre, page, per_page=5):  # all books in the genre
             "date_displayed": b.publish_date_displayed
         })
 
-    return {"books": data,
+    return {"data": data,
         "has_next": pagination.has_next,
         "page": page}
 
@@ -147,7 +147,7 @@ def get_blog_posts(page, per_page=5):
         })
     
 
-    return {"posts":posts,
+    return {"data":posts,
         "has_next": pagination.has_next,
         "page": page}
 
--- frontend/src/public/components/ContentListPage.jsx
+++ frontend/src/public/components/ContentListPage.jsx
@@ -28,7 +28,7 @@ useEffect(() => {
     .then(data => {
       const rawItems = Array.isArray(data)
         ? data
-        : data?.data  ?? [];
+        : data?.data ?? [];
 
       if (!rawItems) {
         console.error("No items found:", data);
