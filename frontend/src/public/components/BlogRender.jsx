9e3451e85a99eb5aab5fec98bcc564dad3798e4d
Kenneth Castro <kencas021@gmail.com>
2026-09-06 16:21:18 -0700

    test

--- frontend/src/public/components/BlogRender.jsx
+++ frontend/src/public/components/BlogRender.jsx
@@ -134,7 +134,9 @@ console.log("BLOG RENDER BLOCKS:", post?.content_blocks);
               {hasMedia && (
                 <div
                   className="image-container"
-
+                  style={{
+                  flex: ratio ? (ratio > 1.2 ? 1.6 : 1) : 1
+                  }}
                 >
                   {renderMedia(src, type, ratioKey)}
 
