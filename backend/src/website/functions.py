bef7d28a61faf15dc7fad797fa7006c8b9904daf
geth2a93 <alecbennardonew@gmail.com>
2026-09-07 08:33:54 -0700

    Update functions.py

--- backend/src/website/functions.py
+++ backend/src/website/functions.py
@@ -2,7 +2,7 @@ from .models import *
 from flask import request
 import re
 from bs4 import BeautifulSoup
-from admin_functions import strip_html
+from .admin_functions import strip_html
 
 def build_url(path):
     if not path:
